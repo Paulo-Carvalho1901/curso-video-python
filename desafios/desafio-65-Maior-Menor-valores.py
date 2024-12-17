abfae6a9c1361e5d24e9d937b5e52208ab053bf1
Paulo Carvalho <paulo.carvalho@global.ntt>
2024-12-17 15:47:01 -0300

    refector

--- desafios/desafio-65-Maior-Menor-valores.py
+++ desafios/desafio-65-Maior-Menor-valores.py
@@ -5,6 +5,4 @@ no final da execução, mostre a média entre todos
 os valores e qual foi o maior e menor valores lido
 o programa deve perguntar ao usuário se ele quer ou não
 continuar a digitar valores
-
-
 """
