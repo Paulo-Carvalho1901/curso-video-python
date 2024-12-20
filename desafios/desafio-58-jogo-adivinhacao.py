75d0b41971f7b883f144f26c3d6270512971fb0e
Paulo Carvalho <paulo.carvalho@global.ntt>
2024-12-19 23:44:25 -0300

    Criando jogoadivinhação

--- desafios/desafio-58-jogo-adivinhacao.py
+++ desafios/desafio-58-jogo-adivinhacao.py
@@ -7,3 +7,20 @@ quantos palpites foram necessarios para vencer
 
 """
 
+from random import randint
+computador = randint(0, 10)
+print('Sou seu computador acabei de pensar em um número entre 0 e 10: ')
+print('Será que consegue adivinhar qual foi? ')
+acertou =  False
+palpites = 0
+while not acertou:
+    jogador = int(input('Qual é seu palpite? '))
+    palpites += 1
+    if jogador == computador:
+        acertou = True
+    else:
+        if jogador < computador:
+            print('Mais!')
+        elif jogador > computador:
+            print('Menos!')
+print(f'Parabéns Acertou! voceê tentou {palpites}x')
--- modulo-02/Aula-10-while.py
+++ modulo-02/Aula-10-while.py
@@ -13,7 +13,7 @@
 # c = 1
 # while c < 10:
 #     print(c)
-#     c += 1
+#     c += 1 
 # print('Fim')
 
 # r = 'S'
