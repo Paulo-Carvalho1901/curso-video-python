ec0e66c1150b83e8e8515e5095671a527dcb2d77
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-01-19 16:56:38 -0300

    for contagem negativa

--- modulo-02/Aula-09-repeticao-for.py
+++ modulo-02/Aula-09-repeticao-for.py
@@ -1,5 +1,10 @@
 # ESTRURA DE VARIÁVEL DE CONTROLE FOR
 
-for c in range(1, 6):
-    print('Ola, mundo!')
-print('fim')
+# for c in range(1, 6):
+#     print('Ola, mundo!')
+# print('fim')
+
+# contando de tras para frente
+for c in range(6, 0, -1):
+    print(c)
+print('Fim')
