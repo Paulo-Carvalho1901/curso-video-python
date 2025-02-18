bf9bb2ac05a0ace0cefe0cd92981d321f455bdc8
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-02-18 01:46:32 -0300

    fatorial

--- desafios/desafio-60-calculo-fatorial.py
+++ desafios/desafio-60-calculo-fatorial.py
@@ -4,3 +4,14 @@ um número qualquer e faça seu fatorial
 
 """
 
+# from math import factorial
+f = 1
+n = int(input('Digite um número para calcular seu fatorial: '))
+c = n
+print(f'Calculando o {n}! = ', end=' ')
+while c > 0:
+    print(f'{c}', end=' ')
+    print('x ' if c > 1 else ' = ', end=' ')
+    f *= c
+    c -= 1
+print(f'{f}')
