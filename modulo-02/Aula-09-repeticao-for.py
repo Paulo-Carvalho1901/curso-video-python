08ecbffa00cdcb4ad4595f99896200fd72beb2b0
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-02-18 00:11:00 -0300

    repetição for

--- modulo-02/Aula-09-repeticao-for.py
+++ modulo-02/Aula-09-repeticao-for.py
@@ -5,6 +5,27 @@
 # print('fim')
 
 # contando de tras para frente
-for c in range(6, 0, -1):
-    print(c)
-print('Fim')
+# for c in range(6, 0, -1):
+#     print(c)
+# print('Fim')
+
+# solicitando um numero for
+# n = int(input('Digite um número: '))
+# for c in range(0, n+1):
+#     print(c)
+    
+# solicitando i, f, f for
+# i = int(input('Digite o inicio: '))
+# f = int(input('Digite o fim: '))
+# p = int(input('Digite o passo: '))
+
+# for c in range(i, f+1, p):
+#     print(c)
+# print('Fim')
+
+# Repetindo o valor, somatório dos mesmos
+soma = 0
+for c in range(1, 5):
+    n = int(input('Digite um valor: '))
+    soma += n
+print(f'O somatório dos valores é {soma}')
