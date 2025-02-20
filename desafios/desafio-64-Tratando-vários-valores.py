5d32604b5f96463d6b20187a940b474a46567696
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-02-20 10:47:36 -0300

    varias numeros soma

--- desafios/desafio-64-Tratando-vários-valores.py
+++ desafios/desafio-64-Tratando-vários-valores.py
@@ -5,5 +5,11 @@ quando o usuário digitar o valor 999, que é a condição
 que parada. No final, mostre quantos números foram
 digitados e qual foi o soma entre eles
 desconsiderandoa flag
-
 """
+numero = cont = soma = 0
+numero = int(input('Digite número 999 para parar! '))
+while numero != 999:
+    soma += numero
+    cont += 1
+    numero = int(input('Digite número 999 para parar! '))
+print(f'Você digitou {cont} números e a soma entre eles é {soma} ')
