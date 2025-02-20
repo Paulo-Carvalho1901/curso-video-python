60c0eb3afe78cbed9800c97b7c1f822b202ab3a4
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-02-20 11:04:40 -0300

    maior e menor numero

--- desafios/desafio-65-Maior-Menor-valores.py
+++ desafios/desafio-65-Maior-Menor-valores.py
@@ -7,3 +7,21 @@ o programa deve perguntar ao usuário se ele quer ou não
 continuar a digitar valores
 
 """
+
+resp = 'S'
+soma = qtd = media = maior = menor = 0
+while resp in 'Ss':
+    num = int(input('Digite um número: '))
+    soma += num
+    qtd += 1
+    if qtd == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = menor
+    resp = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+media = soma / qtd
+print(f'Você digitou {num} numeros e a média foi {media:.2f}')
+print(f'O maior números foi {maior} e o menos números foi {menor}')
