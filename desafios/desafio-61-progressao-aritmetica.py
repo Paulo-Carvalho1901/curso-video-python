1683f780bc09ca4b75345f9817bada4437a06fc6
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-02-20 09:18:56 -0300

    pregressao aritmetica

--- desafios/desafio-61-progressao-aritmetica.py
+++ desafios/desafio-61-progressao-aritmetica.py
@@ -2,6 +2,15 @@
 Refaça o desafio 51, lendo o primeiro
 termo e a razao de uma PA, mostrando 10 primeiros termos da 
 pregressão usando a estrutura while
-
 """
-
+print('Gerador de PA')
+print('=-=' * 10)
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão da PA: '))
+termo = primeiro
+cont = 1
+while cont <= 10:
+    print(f' -> {termo}', end=' ')
+    termo += razao
+    cont += 1
+print('Fim')
