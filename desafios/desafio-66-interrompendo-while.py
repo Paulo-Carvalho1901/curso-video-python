1b754d1e1a825165f390613f7d955b0923df039f
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-02-21 13:30:11 -0300

    interrompendo o while

---
+++ desafios/desafio-66-interrompendo-while.py
@@ -0,0 +1,8 @@
+"""
+Crie um programa que leia varios números inteiro
+pela teclado, o programa só vai parar quando o usuario digitar 999,
+que é a condição da parada. No final, mostre quantos números foram digitardos
+e qual foi a soma entre eles 
+(desconsiderando a flag)=
+"""
+
