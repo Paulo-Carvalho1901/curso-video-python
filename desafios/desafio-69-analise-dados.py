dbd912e35c023ee8e170446fd3a38c640a7e88e2
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-02-21 13:41:53 -0300

    analise de dados

---
+++ desafios/desafio-69-analise-dados.py
@@ -0,0 +1,10 @@
+"""
+crie um programa que leia a idade e o sexo de varias pessoas
+a cada pessoa cadastrada, o programa deverá perguntar se o usuario
+quer ou não continuar. No final, mosttre:
+
+a) quantas pessoas tem mais de 18 anos
+b) quanrtos homens foram cadastrados
+c) quantas mulheres tem menos de 20 anos
+"""
+
