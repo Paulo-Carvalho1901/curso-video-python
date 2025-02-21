933268b75513e71afa9fe0cf340e635325dc93f9
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-02-21 13:46:54 -0300

    analise de produto

---
+++ desafios/desafio-70-estatistica-produto.py
@@ -0,0 +1,10 @@
+"""
+Crie um programa que leia nome e preco 
+de vários produtos. O programa deverá perguntar 
+se o úsuario vai continuar. 
+No final mostre:
+
+a) qual é o total gasto
+b) quantos produtos custam mais de R$1000
+c) qual é o nome do produto mais barato
+"""
