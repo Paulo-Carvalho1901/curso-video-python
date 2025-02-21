5000d106949c3a3c1bdaaeb04ddc2c80f16806ff
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-02-21 13:56:00 -0300

    caixa eletronico

---
+++ desafios/desafio-71-caixa-eletronico.py
@@ -0,0 +1,10 @@
+"""
+Crie um programa que simule funcionamento de um caixa eletronico
+no inicio perhunte ao usuario qual será o valor a ser sacado
+(números inteiro) e o programa vai imformar quantas cedulas de cada valor serão
+entregues
+
+OBS: Considere que o caixa possui cédulas 
+de R$50, R$20, R$10 e R$1
+
+"""
