26ca4adcf67fd43b77faaa6ed2528fcae7ebaf1e
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-02-21 13:33:54 -0300

    tabuada

---
+++ desafios/desafio-67-tabuada.py
@@ -0,0 +1,8 @@
+""""
+Faça um programa que mostre a tabuada
+de varios numeros, um de cada vez para cada
+valor digitado pelo usuario. O programa será
+interrompido quando o números solicitado foi
+negativo
+"""
+
