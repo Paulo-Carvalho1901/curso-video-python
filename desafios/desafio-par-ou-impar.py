0b4f173a7aa02d16b2e1eb192d83a459b40b67b0
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-02-21 13:36:42 -0300

    tabuad

---
+++ desafios/desafio-par-ou-impar.py
@@ -0,0 +1,7 @@
+"""
+Faça um programa que jogue par ou impar
+com o compurador. o jogo só será
+interrompido quando o jogador PERDER,
+mostrando o total de vitórias consecutivas
+que ele conquistou final do jogo
+"""
