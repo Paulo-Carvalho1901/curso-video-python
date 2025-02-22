b65205c406a8f82ed24e6f234e99ce2617621969
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-02-22 00:24:11 -0300

    interrompendo while break

--- desafios/desafio-66-interrompendo-while.py
+++ desafios/desafio-66-interrompendo-while.py
@@ -5,4 +5,12 @@ que é a condição da parada. No final, mostre quantos números foram digitardo
 e qual foi a soma entre eles 
 (desconsiderando a flag)=
 """
-
+print('=-=' * 8, 'Leitor de números', '=-=' * 8)
+soma = cont = 0
+while True:
+    num =  int(input('Digite um números: [999 para parar o programa]: '))
+    if num == 999:
+        break
+    cont += 1
+    soma += num
+print(f'Foram digitados {cont} números e a soma entre eles é {soma}')
