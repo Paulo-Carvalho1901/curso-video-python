0dd910866cc6c66bad5b6e2ad91fab934aec9853
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-02-24 23:45:47 -0300

    inicio tuplas

--- mundo-03/aula16_tuplas.py
+++ mundo-03/aula16_tuplas.py
@@ -1,5 +1,13 @@
 """
-Tuplas - 
-
-
+Tuplas variaveis compostas
 """
+
+lanche = ['amburgue', 'suco', 'pizza', 'pugin']
+
+print(lanche[1:])
+print(lanche[0])
+print(lanche[-4])
+print(len(lanche))
+print()
+for c in lanche:
+    print(c)
