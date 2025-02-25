d0e6effd9248b8d6d7ae0a29f72fa6935a646003
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-02-25 00:15:51 -0300

    tuplas

--- mundo-03/aula16_tuplas.py
+++ mundo-03/aula16_tuplas.py
@@ -1,13 +1,42 @@
 """
 Tuplas variaveis compostas
+AS TUPLAS SÃO IMURAVEIS
+Tuplas sintax = nome + (conteudo)
 """
 
-lanche = ['amburgue', 'suco', 'pizza', 'pugin']
+lanche = ('hamburgue', 'suco', 'pizza', 'pugim', 'batata frita')
+# print(type(lanche))
+# print(lanche[1:3])
+# print(lanche[2:])
+# print((lanche[:2]))
+# print(lanche[3])
+# print(lanche[1:]) 
+# print(lanche[0])
+# print(lanche[-4])
+# print(len(lanche))
+# print()
 
-print(lanche[1:])
-print(lanche[0])
-print(lanche[-4])
-print(len(lanche))
-print()
-for c in lanche:
-    print(c)
+# for c in lanche:
+#     print(f'Eu vou comer {c}')
+# print('Comi pra caramba')
+
+# for cont in range(0, len(lanche)):
+#     print(f'Eu vou comer {lanche[cont]} na posição {cont}')
+# print('Comi pra caramba!')
+
+# for pos, comida in enumerate(lanche):
+#     print(f'Vou comer {comida} na posição {pos}')
+
+# tuplas são imutaveis
+# lanche[1] = 'refrigerante'
+# print(lanche[1])
+
+# print(sorted(lanche))
+# print(lanche)
+
+a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = b + a
+# print(c.count(9))
+print(c)
+print(c.index(8))
