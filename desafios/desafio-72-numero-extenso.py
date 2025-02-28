be4bf646e4b7318532e61a2ae2b2a7afaf6f8645
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-02-28 16:17:24 -0300

    criando condiçao de parada

--- desafios/desafio-72-numero-extenso.py
+++ desafios/desafio-72-numero-extenso.py
@@ -13,3 +13,9 @@ seu programa deverá ler números pelo teclado
 count = ('zero', 'un', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nova', 
          'dez', 'onze', 'doze', 'treze', 'catorze', 'quinze', 'dezesete', 'dezoito', 'dezenove', 'vinte')
 
+# solicitando o números ao usuario e criando a condição de parada
+while True:
+    num = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= num <= 20:
+        break
+    print('Tente novamente. ', end='')
