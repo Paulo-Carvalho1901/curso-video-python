e5f02ec7e84c1b8c90e766dc84de95296a2aad7a
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-02-28 13:17:01 -0300

    init validando dados usuarios

--- desafios/desafio-70-estatistica-produto.py
+++ desafios/desafio-70-estatistica-produto.py
@@ -8,3 +8,17 @@ a) qual é o total gasto
 b) quantos produtos custam mais de R$1000
 c) qual é o nome do produto mais barato
 """
+while True:
+    produto = str(input('Digite o nome do produto:'))
+    preco = float(input('Digite seu preço: '))
+
+    
+
+
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Deseja continuar? [S/N]')).strip().upper()[0]
+    if resp == 'N':
+        break
+
+print('Fim do programa')
