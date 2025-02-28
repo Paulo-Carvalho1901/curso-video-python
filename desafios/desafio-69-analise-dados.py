127a1baa6637eab5ecc09fce1ad420140413589b
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-02-28 12:50:17 -0300

    init entrada de dados para usuario

--- desafios/desafio-69-analise-dados.py
+++ desafios/desafio-69-analise-dados.py
@@ -7,4 +7,12 @@ a) quantas pessoas tem mais de 18 anos
 b) quanrtos homens foram cadastrados
 c) quantas mulheres tem menos de 20 anos
 """
+while True: # Criando loop infinito
+    idade = int(input('Digite sua idade: '))
+    sexo = '' # variavel vazia
+    while sexo not in 'MF': # condição criada para validar o sexo
+        sexo = str(input('Digite seu sexo [F/M]')).strip().upper()[0] # solicitando o sexo apenas a primeira letra
+
+    
+    
 
