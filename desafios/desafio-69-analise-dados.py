9951364f21f0f249fb43794217e904985bd3dd1d
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-02-28 12:57:43 -0300

    criando condicao de parada

--- desafios/desafio-69-analise-dados.py
+++ desafios/desafio-69-analise-dados.py
@@ -9,12 +9,14 @@ c) quantas mulheres tem menos de 20 anos
 """
 while True: # Criando loop infinito
     idade = int(input('Digite sua idade: '))
-    sexo = '' # variavel vazia
+    sexo = ' ' # variavel vazia
     while sexo not in 'MF': # condição criada para validar o sexo
         sexo = str(input('Digite seu sexo [F/M]')).strip().upper()[0] # solicitando o sexo apenas a primeira letra
 
-    resp = ''
+    resp = ' '
     while resp not in 'SN':
         resp = str(input('Quer continuar? [S/N]')).strip().upper()[0]
+    if resp == 'N':
+        break
     
 
