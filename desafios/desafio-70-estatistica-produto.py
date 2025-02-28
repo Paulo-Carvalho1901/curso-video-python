0eab1262890827257e1d4c3ffe4352be3c6fcbed
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-02-28 13:26:06 -0300

    total de produto acima de 1000

--- desafios/desafio-70-estatistica-produto.py
+++ desafios/desafio-70-estatistica-produto.py
@@ -9,12 +9,15 @@ b) quantos produtos custam mais de R$1000
 c) qual é o nome do produto mais barato
 """
 total_compra = 0
+total_mil = 0
 while True:
     produto = str(input('Digite o nome do produto:'))
     preco = float(input('Digite seu preço: '))
     # total da compra
     total_compra += preco
-    
+    # produtos acima de 1000
+    if preco > 1000:
+        total_mil += 1
 
 
     resp = ' '
@@ -24,3 +27,4 @@ while True:
         break
 print('Fim do programa')
 print(f'O total do dos gastos foram R${total_compra} reais')
+print(f'A quantidade de produto que custa mais de R$1000 reais é {total_mil}')
