a39dc11dcecc9357f03299c11400e5ebc5a8c76f
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-02-28 16:12:57 -0300

    init numero extenso

---
+++ desafios/desafio-72-numero-extenso.py
@@ -0,0 +1,15 @@
+"""
+Crie um programa 
+que tenha uma tupla
+totalmente preenchida com uma contagem por
+extenso de zero a vinte
+
+seu programa deverá ler números pelo teclado
+(entre 0 e 20) e mostra-lo por extenso
+
+"""
+
+# inserindo uma tupla
+count = ('zero', 'un', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nova', 
+         'dez', 'onze', 'doze', 'treze', 'catorze', 'quinze', 'dezesete', 'dezoito', 'dezenove', 'vinte')
+
