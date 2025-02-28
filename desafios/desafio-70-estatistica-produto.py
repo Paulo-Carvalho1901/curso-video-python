68a2dcc5f2cfcd18a0738f286e392c25088122be
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-02-28 13:54:17 -0300

    produto mais barato

--- desafios/desafio-70-estatistica-produto.py
+++ desafios/desafio-70-estatistica-produto.py
@@ -10,16 +10,21 @@ c) qual é o nome do produto mais barato
 """
 total_compra = 0
 total_mil = 0
+menor_preco = 0
+cont_produto = 0
+produto_barato = ''
 while True:
     produto = str(input('Digite o nome do produto:'))
     preco = float(input('Digite seu preço: '))
+    cont_produto += 1
     # total da compra
     total_compra += preco
     # produtos acima de 1000
     if preco > 1000:
         total_mil += 1
-
-
+    if cont_produto == 1 or preco < menor_preco:
+        menor_preco = preco
+        produto_barato = produto
     resp = ' '
     while resp not in 'SN':
         resp = str(input('Deseja continuar? [S/N]')).strip().upper()[0]
@@ -28,3 +33,4 @@ while True:
 print('Fim do programa')
 print(f'O total do dos gastos foram R${total_compra} reais')
 print(f'A quantidade de produto que custa mais de R$1000 reais é {total_mil}')
+print(f'O produto mais barato foi {produto_barato} e custa R$ {menor_preco} reais')
