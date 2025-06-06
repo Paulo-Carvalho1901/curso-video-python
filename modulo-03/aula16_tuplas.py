fa8051dc9fe348842d3680ed14239c2a399d9218
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-06-06 16:15:35 -0300

    inicio tuplas algumas ações que se da para fazer com as tuplas

--- modulo-03/aula16_tuplas.py
+++ modulo-03/aula16_tuplas.py
@@ -3,3 +3,21 @@ Tuplas variaveis compostas
 AS TUPLAS SÃO IMURAVEIS
 Tuplas sintax = nome + (conteudo)
 """
+
+lanche = ('Hambúrgue', 'Suco', 'Pizza', 'Pudim')
+print(lanche[1])
+print(lanche[-2])
+print(lanche[1:3]) # efetuando fatiamento
+print(lanche[2:])
+print(lanche[:2])
+print(lanche[-2:])
+print(lanche[-3:])
+
+# TypeError: 'tuple' object does not support item assignment
+# lanche[1] = 'Refrigerante'# tuplas são imútaveis
+
+
+# Iteração sobre as tuplas
+# for comida in lanche:
+    # print(f'Eu vou comer {comida}')
+print(len(lanche))
