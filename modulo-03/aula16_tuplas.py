a880174ab266472a0d25e0868427e43fe19baf9c
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-06-07 19:32:49 -0300

    refeito a aula 16 com tuplas

--- modulo-03/aula16_tuplas.py
+++ modulo-03/aula16_tuplas.py
@@ -4,14 +4,14 @@ AS TUPLAS SÃO IMURAVEIS
 Tuplas sintax = nome + (conteudo)
 """
 
-lanche = ('Hambúrgue', 'Suco', 'Pizza', 'Pudim')
-print(lanche[1])
-print(lanche[-2])
-print(lanche[1:3]) # efetuando fatiamento
-print(lanche[2:])
-print(lanche[:2])
-print(lanche[-2:])
-print(lanche[-3:])
+lanche = ('Hambúrgue', 'Suco', 'Pizza', 'Pudim', 'Batata Frita')
+# print(lanche[1])
+# print(lanche[-2])
+# print(lanche[1:3])
+# print(lanche[2:])
+# print(lanche[:2])
+# print(lanche[-2:])
+# print(lanche[-3:])
 
 # TypeError: 'tuple' object does not support item assignment
 # lanche[1] = 'Refrigerante'# tuplas são imútaveis
@@ -20,4 +20,31 @@ print(lanche[-3:])
 # Iteração sobre as tuplas
 # for comida in lanche:
     # print(f'Eu vou comer {comida}')
-print(len(lanche))
+
+
+# for comida in lanche:
+#     print(comida)
+
+
+# for cont in range(0, len(lanche)):
+#     print(cont, lanche[cont])
+
+
+# for pos, comida in enumerate(lanche):
+#     print(f'Comi muito {comida}, na posição {pos}')
+
+# print(cont) # contador
+# print(lanche) # tupla de lanche
+
+# print(sorted(lanche))
+
+a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = a + b
+
+print(c)
+print(c.index(2))
+
+pessoa = ('Paulo', 36, 'M', 92.80)
+
+print(pessoa)
