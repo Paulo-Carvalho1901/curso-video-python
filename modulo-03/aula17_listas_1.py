1eb032c40c6fe836c0c2e77d02f6e447d0705157
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-15 14:34:46 -0300

    addicionando mais algumas funções e exercicios na lista

--- modulo-03/aula17_listas_1.py
+++ modulo-03/aula17_listas_1.py
@@ -1,5 +1,22 @@
 # Listas -  as listas são mutaveis
 # criando uma lista
+
+num = [2, 5, 9, 1] # lista
+num[2] = 3 # adicionando intem na lista
+num.append(7) # adicionando item na lista utilmo indice
+num.sort(reverse=True) # revertendo a ordem da lista
+num.insert(2, 2) # inserindo item na lista de acordo com indice
+# num.pop(2) # removendo item na lista de acordo com indice e se não for passado indice remove o ultimo elemento
+# num.remove(4) # remove ele procura do inicio na lista aprimeira ocorrencia e remove
+if 5 in num:
+    num.remove(5)
+else:
+    print('Não achei o número 4!')
+print(num)
+# print(f'Essa lista tem len{num} elementos.')
+
+print()
+
 palavras = ['programar', 'computador', 'trabalho', 'tecnologia']
 	
 # alterando uma lista
@@ -33,4 +50,20 @@ print(lista)
 lista.sort(reverse=True)
 print(lista)
 print(len(lista))
+
+print()
+print('Novos valores da lista')
+
+valores = []
+
+for count in range(0, 5):
+    valores.append(int(input('Digite os valores para lista: ')))
+
+valores.append(5)
+valores.append(9)
+valores.append(4)
+
+for c, v in enumerate(valores):
+    print(f'Na posição {c} encontrei o valor {v}!')
+print('Cheguei ao final na lista!')
 
