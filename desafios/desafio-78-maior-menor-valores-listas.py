0b0700c3d200ee00f9a9a573876ed46d06f3d6ce
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-16 09:48:36 -0300

    criado condição para maior  e menor valor

--- desafios/desafio-78-maior-menor-valores-listas.py
+++ desafios/desafio-78-maior-menor-valores-listas.py
@@ -17,5 +17,12 @@ menor = 0
 for count in range(0, 5):
     listas.append(int(input(f'Digite um valor para a posiççao {count}: ')))
 
+    # criado condição para verificar maior e menor valor
+    if count == 0:
+        maior = menor = listas[count]
+    else:
+        if listas[count] > maior:
+            maior = listas[count]
+        if listas[count] < menor:
+            menor = listas[count]  
 print('=-=' * 30)
-print(f'Você digitou os valores {listas}')
