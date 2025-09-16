e5af91575e361828c44e487c54ff2a8b3bd5dc9e
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-16 16:33:33 -0300

    ordenando lista em ordem sen a função sort

--- desafios/desafio-80-lista-ordenada-sem-repeticao.py
+++ desafios/desafio-80-lista-ordenada-sem-repeticao.py
@@ -6,26 +6,18 @@ lista, já na posição correta de inserção (sem usar sort())
 No final, mostre a lista ordenada na tala
 """
 
-# def ordenar_lista_sem_sort(lista):
-#     n = len(lista)
-#     lista_ordenada = lista[:]  # Cria uma cópia da lista original para não modificá-la
+lista = []
 
-#     for i in range(n):
-#         # Encontra o índice do menor elemento no restante da lista
-#         indice_menor = i
-#         for j in range(i + 1, n):
-#             if lista_ordenada[j] < lista_ordenada[indice_menor]:
-#                 indice_menor = j
-
-#         # Troca o menor elemento com o elemento na posição i
-#         lista_ordenada[i], lista_ordenada[indice_menor] = lista_ordenada[indice_menor], lista_ordenada[i]
-
-#     return lista_ordenada
-
-# # Exemplo de uso:
-# minha_lista = [5, 2, 8, 1, 9, 4]
-# lista_ordenada = ordenar_lista_sem_sort(minha_lista)
-# print(f"Lista original: {minha_lista}")
-# print(f"Lista ordenada: {lista_ordenada}")
-
-
+for count in range(0, 5):
+    n = int(input('Digite um valor: '))
+    if count == 0 or n > lista[-1]:
+        lista.append(n)
+    else:
+        pos = 0
+        while pos < len(lista):
+            if n <= lista[pos]:
+                lista.insert(pos, n)
+                break
+            pos += 1
+print('=-' * 30)
+print(f'Os valores digitados em ordem foram {lista}')
