09bd3c9932552bc343cd81667095c780f0ada7cc
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-16 14:34:30 -0300

    inicio da resolução do problema

---
+++ desafios/desafio-79-valores-unico-lista.py
@@ -0,0 +1,14 @@
+"""
+Crie uma programa que usúario possa digite
+varios valores númericos e cadastra-os em uma
+lista. Caso número já exista lá dentro. ele
+não será adicionado. No final, serão exibidos todos os
+valores únicos digitados, em ordem crescente.
+"""
+
+# criando a lista vazia que sera populada
+numeros = []
+
+# criando um laço while, que terá uma condição de parada
+while True:
+    ...
