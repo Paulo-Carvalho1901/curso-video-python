d11606c8bd9bca63823444dc5251e39a1183c1f9
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-16 14:38:47 -0300

    adicionado numeros na lista

--- desafios/desafio-79-valores-unico-lista.py
+++ desafios/desafio-79-valores-unico-lista.py
@@ -11,4 +11,12 @@ numeros = []
 
 # criando um laço while, que terá uma condição de parada
 while True:
-    ...
+    # solicitando ao usuario um valor inteiro
+    n = int(input('Digite um valor: '))
+    # checando se os numeros solicitados esta na lista
+    if n not in numeros:
+        numeros.append(n) # linha que adiciona os numeros na lista
+        print('Número adicionado com sucesso!')
+    else:
+        print('Número duplicado, não vou adicionar!')
+    
