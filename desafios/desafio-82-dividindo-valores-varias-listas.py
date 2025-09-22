0fffb1006eee3d33bd6d44240896598f156a1f8c
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-22 11:50:46 -0300

    separando os valores de pares e impares

--- desafios/desafio-82-dividindo-valores-varias-listas.py
+++ desafios/desafio-82-dividindo-valores-varias-listas.py
@@ -19,5 +19,12 @@ while True:
     res = str(input('Gostaria de continuar? [S/N]: '))
     if res in 'Nn':
         break
-
+for i, v in enumerate(lista_mae):
+    if v % 2 == 0:
+        lista_par.append(v)
+    elif v % 2 == 1:
+        lista_impar.append(v)
+print('-=' * 30)
 print(f'Minha lista completa é {lista_mae}')
+print(f'Minha lista de pares é {lista_par}')
+print(f'Minha lista de impares é {lista_impar}')
