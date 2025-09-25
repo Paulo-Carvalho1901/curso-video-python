f51839e8f34ab4d80446fafeb4bf28b2201024e6
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-25 09:36:06 -0300

    analisando os dados por maior e menor de idade

--- modulo-03/aula18_lista_2.py
+++ modulo-03/aula18_lista_2.py
@@ -26,9 +26,22 @@ galera = []
 dados = list()
 
 # solicitando dados ao usuario com for
+total_maior = total_menor = 0
 for c in range(0, 3):
     dados.append(str(input('Nome: ')))
     dados.append(int(input('Idade: ')))
     galera.append(dados[:]) # fazendo uma copia raza dos dados em galera
     dados.clear()
 print(galera)
+
+# exibindo com condicionais
+
+for p in galera:
+    if p[1] >= 21:
+        print(f'{p[0]} é maior de idade.')
+        total_maior += 1
+    else:
+        print(f'{p[0]} é menor de idade.')
+        total_menor += 1
+
+print(f'Temos {total_maior} maiore e {total_menor} menor de idade')
