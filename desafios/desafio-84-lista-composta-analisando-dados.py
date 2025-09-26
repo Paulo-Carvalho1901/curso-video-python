b4bff870d50d55f66e2c413deb0b1af3487898b4
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-26 10:34:50 -0300

    Resolvido o b) Uma listagem das pessoas mais pessadas

--- desafios/desafio-84-lista-composta-analisando-dados.py
+++ desafios/desafio-84-lista-composta-analisando-dados.py
@@ -11,15 +11,23 @@ c) Uma listagem com os pessoas mais leves
 
 temp = []
 princ = []
-
+maior = menor = 0
 while True:
     temp.append(str(input('Nome: ')))
     temp.append(float(input('Peso: ')))
+    if len(princ) == 0:
+        maior = menor = temp[1]
+    else:
+        if temp[1] > maior:
+            maior = temp[1]
+        if temp[1] < menor:
+            menor = temp[1]
     princ.append(temp[:])
     temp.clear()
     res = str(input('Quer continuar? [S/N]: '))
     if res in 'Nn':
         break
 print('=-' * 30)
-print(f'Os dados foram {princ}')
 print(f'Ao todo você cadastrou {len(princ)} pessoas')
+print(f'O maior peso foi de {maior}kg.')
+print(f'O menor peso foi de {menor}Kg.')
