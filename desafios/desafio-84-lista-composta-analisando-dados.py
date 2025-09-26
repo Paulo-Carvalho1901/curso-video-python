46a8ea7afb90296e65265962b4ac50f19f2d91c9
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-26 10:27:28 -0300

    primeiraparte do problema resolvido

---
+++ desafios/desafio-84-lista-composta-analisando-dados.py
@@ -0,0 +1,24 @@
+"""
+Faça um programa que leia nome e peso
+de varias pessoas, guardando tudo em uma lista.
+No final, mostre:
+
+a) Quantas pessoas from cadastradas
+b) Uma listagem das pessoas mais pessadas
+c) Uma listagem com os pessoas mais leves
+
+"""
+
+temp = []
+princ = []
+
+while True:
+    temp.append(str(input('Nome: ')))
+    temp.append(float(input('Peso: ')))
+    princ.append(temp[:])
+    temp.clear()
+    res = str(input('Quer continuar? [S/N]: '))
+    if res in 'Nn':
+        break
+print('=-' * 30)
+print(f'Os dados foram {princ}')
