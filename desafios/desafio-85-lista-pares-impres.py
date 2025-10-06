eedbb5c929ef58b6a1ace90e19f101872ac1318f
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-06 19:14:56 -0300

    ordenado os numeros

--- desafios/desafio-85-lista-pares-impres.py
+++ desafios/desafio-85-lista-pares-impres.py
@@ -15,5 +15,7 @@ for c in range(1, 8):
     else:
         num[1].append(valor)
 print('-=' * 30)
+num[0].sort()
+num[1].sort()
 print(f'Todos os valores digitado pares são: {num[0]}')
 print(f'Todos os valores digitado ímpares são: {num[1]}')
