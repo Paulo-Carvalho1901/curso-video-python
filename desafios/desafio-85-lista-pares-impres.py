80c28e2b42fbd3d657ac24bebba34d930bf32483
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-06 19:06:50 -0300

    criando logica para separar os valores de par e impar

--- desafios/desafio-85-lista-pares-impres.py
+++ desafios/desafio-85-lista-pares-impres.py
@@ -10,3 +10,8 @@ valor = 0
 
 for c in range(1, 8):
     valor = int(input('Digite um valor: '))
+    if valor % 2 == 0:
+        num[0].append(valor)
+    else:
+        num[1].append(valor)
+        
