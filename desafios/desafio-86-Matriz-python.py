256fbf9cbfaefcc1951e56d90188a143412631df
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-06 22:44:43 -0300

    inicio da matriz

--- desafios/desafio-86-Matriz-python.py
+++ desafios/desafio-86-Matriz-python.py
@@ -7,3 +7,9 @@ No final, mostre o valores na tela
 com a formatação correta.
 """
 
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for l in range(0, 3):
+    for c in range(0, 3):
+        matriz[l][c] = int(input('Digite um valor: '))
+
+print(matriz)
