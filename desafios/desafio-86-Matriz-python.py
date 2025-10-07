4d04251dfa8d9d0c6c207833b1c73922811000c9
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-06 23:24:57 -0300

    imprimindo a solução da matriz

--- desafios/desafio-86-Matriz-python.py
+++ desafios/desafio-86-Matriz-python.py
@@ -10,6 +10,10 @@ com a formatação correta.
 matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 for l in range(0, 3):
     for c in range(0, 3):
-        matriz[l][c] = int(input('Digite um valor: '))
+        matriz[l][c] = int(input(f'Digite um valor para {l}, {c}: '))
 
-print(matriz)
+print('-=' * 30)
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f'[{matriz[l][c]:^5}]', end='')
+    print()
